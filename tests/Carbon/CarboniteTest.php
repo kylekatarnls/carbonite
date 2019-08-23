230c1b2fdb5380cc7919e04fe51eee9acd93da20
KyleKatarn <kylekatarnls@gmail.com>
2019-08-23 18:42:40 +0200

    Add fake() tests

--- tests/Carbon/CarboniteTest.php
+++ tests/Carbon/CarboniteTest.php
@@ -12,8 +12,17 @@ use PHPUnit\Framework\TestCase;
  */
 class CarboniteTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Carbonite::mock(null);
+        Carbonite::release();
+    }
+
     /**
      * @covers ::mock
+     * @covers ::tibanna
      * @covers \Carbon\Carbonite\Tibanna::mock
      */
     public function testMock()
@@ -25,5 +34,26 @@ class CarboniteTest extends TestCase
         Carbonite::mock(null);
 
         self::assertLessThan(1, abs(Carbon::now()->format('U.u') - (new DateTimeImmutable('now'))->format('U.u')));
+    }
+
+    /**
+     * @covers ::fake
+     * @covers \Carbon\Carbonite\Tibanna::fake
+     */
+    public function testFake()
+    {
+        $realNow = Carbon::instance(new DateTimeImmutable('now'));
+        $fakeNow = Carbonite::fake($realNow);
+
+        self::assertGreaterThan($realNow->format('Y-m-d H:i:s.u'), $fakeNow->format('Y-m-d H:i:s.u'));
+        self::assertLessThan(1, abs($fakeNow->format('U.u') - $realNow->format('U.u')));
+
+        Carbonite::mock($realNow);
+        Carbonite::freeze($realNow);
+        $fakeNow = Carbonite::fake($realNow);
+
+        self::assertSame($realNow->format('Y-m-d H:i:s.u'), Carbon::now()->format('Y-m-d H:i:s.u'));
+
+        Carbonite::mock(null);
     }
 }
